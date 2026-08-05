2c1be0c1d0f8b01d986ecee4e74727bec16ba7b9
Muhammad Huzaifa <muhammadhuzaifa@FixIT.Local>
2026-08-06 00:58:13 +0500

    Add a VideoAnimationScene to replace Scene & Add Video Instead of Glitchy Puppet Animation.

--- src/screens/ClimbScreen.js
+++ src/screens/ClimbScreen.js
@@ -3,13 +3,17 @@ import { StyleSheet, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Hud from '../components/Hud';
 import Scene from '../components/Scene';
+import VideoAnimationScene from '../components/VideoAnimationScene';
+
 
 export default function ClimbScreen({ running, lifetime, durationMs, readElapsed, onComplete }) {
   const insets = useSafeAreaInsets();
 
   return (
     <View style={styles.root}>
-      <Scene running={running} />
+      {/* Keeping the old code as it is, so we can compares the old & current snapshots any time we want. */}
+      {/* <Scene running={running} /> */}
+      <VideoAnimationScene running={running} />
       <View style={[styles.hud, { paddingTop: insets.top + 10 }]}>
         <Hud
           lifetime={lifetime}
